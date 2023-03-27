7376ba16d88a39b8e60a7219f5053faaa53406ee
Lucas Lopes <lucas.lopesx1@gmail.com>
2023-03-27 19:28:37 -0300

    feat: findAll users example

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
   findOne(id: number) {
