b348986b970d8f8bdef3e9a9943639089bb70d68
lucaslopes <lucas.lopesx1@gmail.com>
2023-04-02 23:50:19 -0300

    feat: swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Psychology API')
+    .setDescription('The Psychology API description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
-  console.log('http://localhost:3000/');
+  console.log('http://localhost:3000/api');
 }
 bootstrap();
