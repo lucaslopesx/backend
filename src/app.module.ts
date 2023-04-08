7ea0317321d3d7b4665f6bf75b8b2e7df98a59dc
lucaslopes <lucas.lopesx1@gmail.com>
2023-04-08 13:12:00 -0300

    feat: clients controller init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PsychologistModule } from './psychologist/psychologist.module';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [UsersModule, PsychologistModule],
+  imports: [UsersModule, PsychologistModule, ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
