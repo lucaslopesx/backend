dcf80b7b50a5dea46529f4bcf52b4f3d0250f339
lucaslopes <lucas.lopesx1@gmail.com>
2023-04-08 14:29:36 -0300

    feat: clients base controller

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -8,16 +8,15 @@ import {
   Delete,
 } from '@nestjs/common';
 import { ClientsService } from './clients.service';
-import { CreateClientDto } from './dto/create-client.dto';
-import { UpdateClientDto } from './dto/update-client.dto';
+import { Prisma } from '@prisma/client';
 
 @Controller('clients')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
 
   @Post()
-  create(@Body() createClientDto: CreateClientDto) {
-    return this.clientsService.create(createClientDto);
+  create(@Body() client: Prisma.ClientCreateInput) {
+    return this.clientsService.create(client);
   }
 
   @Get()
@@ -31,8 +30,8 @@ export class ClientsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateClientDto: UpdateClientDto) {
-    return this.clientsService.update(+id, updateClientDto);
+  update(@Param('id') id: string, @Body() client: Prisma.ClientUpdateInput) {
+    return this.clientsService.update(+id, client);
   }
 
   @Delete(':id')
--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import { CreateClientDto } from './dto/create-client.dto';
-import { UpdateClientDto } from './dto/update-client.dto';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class ClientsService {
-  create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+  constructor(private prisma: PrismaService) {}
+
+  create(client: Prisma.ClientCreateInput) {
+    return this.prisma.client.create({
+      data: client,
+    });
   }
 
   findAll() {
-    return `This action returns all clients`;
+    return this.prisma.client.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} client`;
+    return this.prisma.client.findUnique({
+      where: {
+        id: id,
+      },
+    });
   }
 
-  update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
+  update(id: number, client: Prisma.ClientUpdateInput) {
+    return this.prisma.client.update({
+      data: client,
+      where: {
+        id: id,
+      },
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} client`;
+    return this.prisma.client.delete({
+      where: {
+        id: id,
+      },
+    });
   }
 }
