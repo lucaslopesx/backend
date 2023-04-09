e17ffd279d0add3bd37462f4f21fbd1f492e839c
lucaslopes <lucas.lopesx1@gmail.com>
2023-04-09 18:05:14 -0300

    removed target-audience

--- src/target-audiences/dto/update-target-audience.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTargetAudienceDto } from './create-target-audience.dto';
-
-export class UpdateTargetAudienceDto extends PartialType(CreateTargetAudienceDto) {}
