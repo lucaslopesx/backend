74398f504ed75daff468b9fa6baba36a2b2490fb
lucaslopes <lucas.lopesx1@gmail.com>
2023-05-05 22:45:38 -0300

    fix: prisma service missing for segment of activity

--- src/segment-of-activity/segment-of-activity.module.ts
+++ src/segment-of-activity/segment-of-activity.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SegmentOfActivityService } from './segment-of-activity.service';
 import { SegmentOfActivityController } from './segment-of-activity.controller';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   controllers: [SegmentOfActivityController],
-  providers: [SegmentOfActivityService]
+  providers: [SegmentOfActivityService, PrismaService],
 })
 export class SegmentOfActivityModule {}
