d2eb8a8dc12f4563385ebcada658daba54eaf3ea
lucaslopesx <lucas.lopesx1@gmail.com>
2023-05-13 15:38:58 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,15 @@ import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-cl
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new PrismaClientExceptionFilter());
+  // app.useGlobalFilters(new PrismaClientExceptionFilter());
 
-  const config = new DocumentBuilder()
-    .setTitle('Psychology API')
-    .setDescription('The Psychology API description')
-    .setVersion('0.1')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('Psychology API')
+  //   .setDescription('The Psychology API description')
+  //   .setVersion('0.1')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
   console.log('http://localhost:3000/api');
